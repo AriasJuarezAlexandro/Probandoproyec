48dd22663a67e714a6513576cba7ab9e1e3ea4fb
AriasJuarezAlexandro <ariasjuarezalex@gmail.com>
2022-04-19 10:57:44 -0500

    cambios01

--- src/main/java/com/example/demo/ProbandoproyecApplication.java
+++ src/main/java/com/example/demo/ProbandoproyecApplication.java
@@ -8,6 +8,7 @@ public class ProbandoproyecApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProbandoproyecApplication.class, args);
+		System.out.println("Hola");
 	}
 
 }
